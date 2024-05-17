585d58abf9a4f58cbd3924544c8c8820250dce9c
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-05-17 15:28:16 -0600

    Add content to phone book app

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,6 @@ const resources = {
     translation: {
       firstName: "First Name",
       lastName: "Last Name",
-      street: "Street",
       // Other translations can go here
     },
   },
@@ -16,7 +15,6 @@ const resources = {
     translation: {
       firstName: "Nombre",
       lastName: "Apellido",
-      street: "Calle",
       // Other translations can go here
     },
   },
