ce57af7e62a9662c40915bd3567924a764410711
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-05-26 13:18:32 -0600

    Add content to phone book app

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,6 +2,8 @@
 import React from "react";
 import "./ContactList.css";
 
+// Components are independent and reusable bits of code. They serve the same purpose
+// as JavaScript functions, but work in isolation and return HTML
 const ContactList = ({ contacts, onDelete, onEdit }) => {
   return (
     <div>
@@ -25,14 +27,17 @@ const ContactList = ({ contacts, onDelete, onEdit }) => {
           <tbody>
             {contacts.map((contact) => (
               <tr key={contact.id}>
-                <td>{contact.firstName}</td>
                 <td>{contact.lastName}</td>
+                <td>{contact.firstName}</td>
                 <td>{contact.phoneNumber}</td>
                 <td>{contact.email}</td>
                 <td>
                   {contact.street}, {contact.city}, {contact.state},{" "}
                   {contact.postal_code}, {contact.country}
                 </td>
+                {/* <td>{contact.company}</td>
+                <td>{contact.job_title}</td>
+                <td>{contact.notes}</td> */}
                 <td>{contact.company}</td>
                 <td>{contact.job_title}</td>
                 <td>{contact.notes}</td>
